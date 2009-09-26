a32154e2f4f08643deba49601563ec2525dddd9e
Marcos Dione <mdione@grulic.org.ar>
2009-09-26 19:48:10 +0200

    added:   search_entry.py + a KLineEdit that empties on Esc.

---
+++ search_entry.py
@@ -0,0 +1,18 @@
+# vim: set fileencoding=utf-8 :
+# (c) 2009 Marcos Dione <mdione@grulic.org.ar>
+# distributed under the terms of the GPLv2.1
+
+# qt/kde related
+from PyKDE4.kdeui import KLineEdit
+from PyQt4.QtCore import QEvent, pyqtSignal, Qt
+
+class SearchEntry (KLineEdit):
+
+    def keyPressEvent (self, event):
+        if event.type ()==QEvent.KeyPress and event.key ()==Qt.Key_Escape:
+            print "esc!"
+            self.setText ('')
+        else:
+            KLineEdit.keyPressEvent (self, event)
+
+# end
