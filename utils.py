a59f023e375c5ae8b33d46976155984b01161977
Marcos Dione <mdione@grulic.org.ar>
2009-11-18 20:46:01 +0100

    modified:   utils.py * set the 'file' scheme when needed (older phonon versions).

--- utils.py
+++ utils.py
@@ -17,7 +17,9 @@
 # along with satyr.  If not, see <http://www.gnu.org/licenses/>.
 
 # qt/kde related
+from PyKDE4.phonon import Phonon
 from PyQt4.QtCore import QByteArray, QUrl
+
 
 def qstring2path (qs):
     # BUG: this is ugly; might be properly handled w/PyQt4.6/Python2.6
@@ -31,7 +33,10 @@ def path2qurl (path):
     # path= '/home/mdione/media/music/Patricio Rey Y Sus Redonditos De Ricota/\xc3\x9altimo bondi a Finisterre/07- La peque\xf1a novia del carioca.wav'
     qba= QByteArray (path)
     qu= QUrl.fromEncoded (qba.toPercentEncoding ("/ "))
-    qu.setScheme ('file')
+    majV, minV, patchL= map (int, Phonon.phononVersion ().split ('.'))
+    # older versions need this
+    if (majV<4) or (majV==4 and minV<3) or (majV==4 and minV==3 and patchL<51):
+        qu.setScheme ('file')
 
     return qu
 
