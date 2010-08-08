070322356972e3642a5bc3cfe9ea8ea66fe5ffdf
Marcos Dione <mdione@grulic.org.ar>
2010-08-08 14:09:53 +0200

    modified:   satyr/skins/renamer.py * finished implementing rename(). KIO takes care of dst already existing.

--- satyr/skins/renamer.py
+++ satyr/skins/renamer.py
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with satyr.  If not, see <http://www.gnu.org/licenses/>.
 
+from PyKDE4.kio import KIO
+from PyKDE4.kdecore import KUrl
+
 from satyr.common import ConfigurableObject
 from satyr import utils
 
@@ -30,6 +33,8 @@ class Renamer (ConfigurableObject):
             ('vaFormat', unicode, u"{%4year - }{%album/}{Disk %02disk/}{%02trackno - }{%artist - }{%title}"),
             )
         self.loadConfig ()
+
+        self.jobs= []
 
     def songPath (self, base, song):
         # TODO: take ext from file format?
@@ -47,13 +52,30 @@ class Renamer (ConfigurableObject):
 
         return ans
 
+    def jobFinished (self, job):
+        print "Renamer.jobFinished()", job, job.error ()
+        try:
+            self.jobs.remove (job)
+        except ValueError:
+            print "Renamer.jobFinished()", job, "not found!"
+
     def rename (self, songs):
         # TODO: parametrize the main music colleciton
         mainColl= self.collaggr.collections[0]
-        path= mainColl.path
+        base= mainColl.path
 
         for song in songs:
-            print "Renamer.rename()", song.filepath, "->", self.songPath (path, song)
+            print "Renamer.rename()", song.filepath, "->", self.songPath (base, song)
+            src= KUrl (song.filepath)
+            dst= KUrl (self.songPath (base, song))
+
+            # TODO: do not launch them all in parallel
+            job= KIO.move (src, dst)
+            # TODO: emit a finished.
+
+            print "Renamer.rename()", job
+            job.result.connect (self.jobFinished)
+            self.jobs.append (job)
 
     def delete (self, songs):
         # TODO: parametrize the trash music colleciton
