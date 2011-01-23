9c6191ecb99da557304b14433ba4f8715c3a7b28
Marcos Dione <mdione@grulic.org.ar>
2011-01-23 12:50:31 +0100

    modified:   satyr/asyncqueue.py + insert() allows to put things fefore others. - don't call the signal, the object should do that by itself (and Song does). * an empty queue doesn't mean we must terminate it. calling stop() is the only way to do it. this allows us to reuse an empty queue.

--- satyr/asyncqueue.py
+++ satyr/asyncqueue.py
@@ -18,6 +18,15 @@ class AsyncQueue (Thread):
         self.sem.release ()
         self.lock.release ()
 
+    def insert (self, *args):
+        self.lock.acquire ()
+        print "AQ.insert(): lock!..."
+        self.queue.insert (0, args)
+        print "AQ.insert(): ... and loaded!"
+        self.count+= 1
+        self.sem.release ()
+        self.lock.release ()
+
     def run (self):
         # for obj, methodName, args, kwargs, signalName in self.queue
         print "AQ.run(): start!"
@@ -32,25 +41,26 @@ class AsyncQueue (Thread):
                 try:
                     method= getattr (obj, methodName)
                     print "AQ.run(): got method"
-                    signal= getattr (obj, signalName)
-                    print "AQ.run(): got signal"
+                    # signal= getattr (obj, signalName)
+                    # print "AQ.run(): got signal"
                 except AttributeError:
                     print "foo"
                 else:
                     try:
-                        value= method (*args, **kwargs)
                         print "AQ.run(): method()"
-                        signal.emit (value)
-                        print "AQ.run(): signal emited!"
+                        value= method (*args, **kwargs)
+                        # signal.emit (value)
+                        # print "AQ.run(): signal emited!"
                     except Exception, e:
                         print "bar: %s" % e
             else:
-                print "AQ.run(): nothing in the q, stopping"
-                self.finish= True
+                print "AQ.run(): nothing in the q"
+                # self.finish= True
 
         print "AQ.run(): finished!"
 
     def stop (self):
+        self.finish= True
         self.sem.release ()
 
 # end
