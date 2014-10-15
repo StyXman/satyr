ebd048e78766d0391bd40c897d4af16865b3520b
Marcos Dione <mdione@grulic.org.ar>
2014-10-15 10:11:48 +0200

    [+] create a tests file that should not be indexed.

--- mk_test_files.sh
+++ mk_test_files.sh
@@ -9,3 +9,6 @@ sox --null tests/src/01-null.mp3 trim 0 10
 cp tests/src/01-null.mp3 tests/src/02-tags.mp3
 # artist, year, collection, diskno, album, trackno, title, length
 python tag_set.py tests/src/02-tags.mp3 'Foo' '1977' '' 0 'Fighters' 2 'Tags' 10
+
+# should not be indexed
+touch tests/src/03-not_index.txt
