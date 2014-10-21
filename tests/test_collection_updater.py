113b2f1eb2314ff988b6c30567b2c0ab07876830
Marcos Dione <mdione@grulic.org.ar>
2014-10-21 19:22:30 +0200

    [+] also test that removing a file is not notified.

--- tests/test_collection_updater.py
+++ tests/test_collection_updater.py
@@ -47,7 +47,7 @@ class TestCollectionUpdater (unittest.TestCase):
         for item in l:
             self.n.remove (item)
 
-    def test_new_file (self):
+    def test_new_song (self):
         dst= os.path.join (test_path, '01-null.mp3')
 
         def copy_file ():
@@ -61,7 +61,7 @@ class TestCollectionUpdater (unittest.TestCase):
 
         self.assertEqual (len (self.n), 1)
 
-    def test_new_wrong_file (self):
+    def test_new_file (self):
         dst= os.path.join (test_path, '03-do_not_index.txt')
 
         def copy_file ():
@@ -75,7 +75,7 @@ class TestCollectionUpdater (unittest.TestCase):
 
         self.assertEqual (len (self.n), 0)
 
-    def test_new_del_file (self):
+    def test_new_del_song (self):
         dst= os.path.join (test_path, '01-null.mp3')
 
         def copy_file ():
@@ -84,6 +84,26 @@ class TestCollectionUpdater (unittest.TestCase):
 
         def del_file ():
             self.assertEqual (len (self.n), 1)
+            unlink (dst)
+
+        self.col= CollectionUpdater (test_path)
+        QTimer.singleShot (0, copy_file)
+        self.col.foundSongs.connect (self.inc)
+        self.col.deletedSongs.connect (self.dec)
+        QTimer.singleShot (1000, app.quit)
+        app.exec_ ()
+
+        self.assertEqual (len (self.n), 0)
+
+    def test_new_del_file (self):
+        dst= os.path.join (test_path, '03-do_not_index.txt')
+
+        def copy_file ():
+            copy ('tests/src/03-do_not_index.txt', dst)
+            QTimer.singleShot (500, del_file)
+
+        def del_file ():
+            self.assertEqual (len (self.n), 0)
             unlink (dst)
 
         self.col= CollectionUpdater (test_path)
